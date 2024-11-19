8d08b64a9644ba952b7b804b044adf588f8d9595
sajalpaudyal <paudyalsajal@gmail.com>
2024-11-19 23:21:50 +0100

    api: get individual house

--- src/api/house.route.js
+++ src/api/house.route.js
@@ -1,16 +1,33 @@
 // api/house.routes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { connectDB } = require('../utils/db');
+const { connectDB } = require("../utils/db");
 
-router.get('/', async (req, res) => {
-    try {
-        const db = await connectDB();
-        const houses = await db.collection('Houses').find({}).toArray();
-        res.json(houses);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+router.get("/", async (req, res) => {
+  try {
+    const db = await connectDB();
+    const houses = await db.collection("Houses").find({}).toArray();
+    res.json(houses);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:houseName", async (req, res) => {
+  try {
+    const db = await connectDB();
+    const house = await db.collection("Houses").findOne({
+      houseName: req.params.houseName,
+    });
+
+    if (house) {
+      res.json(house);
+    } else {
+      res.status(404).json({ message: "House not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
